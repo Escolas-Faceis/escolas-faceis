3d604afa53187b8e96cde1aca9622c7139e0f605
Ana Carolina Duarte Landim <rm90430@estudante.fieb.edu.br>
2025-05-22 12:01:19 -0300

    commitando pastas novas

--- config/pool_conexoes.js
+++ config/pool_conexoes.js
@@ -18,4 +18,5 @@ pool.getConnection((err, conn) => {
         console.log("Conectado ao SGBD!")
 })
 
-module.exports = pool.promise()
+module.exports = pool.promise()git commit -m "Primeiro commit"
+//teste
