b3f36c0555f337e3f1d1eb1a0f0cc96c07f886b7
Ana Carol :D <rm90430@estudante.fieb.edu.br>
2025-10-05 14:53:40 -0300

    clever cloud

--- config/script2.sql
+++ config/script2.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS escolas_faceis;
 CREATE DATABASE IF NOT EXISTS escolas_faceis;
 USE escolas_faceis;
 
@@ -11,31 +12,36 @@ CREATE TABLE imagens (
 DROP TABLE IF EXISTS usuarios;
 CREATE TABLE usuarios (
     id_usuario INT PRIMARY KEY AUTO_INCREMENT,
-    nome_usuario NVARCHAR(100) NOT NULL,
-    email_usuario NVARCHAR(100) NOT NULL UNIQUE,
-    telefone_usuario NVARCHAR(20) NULL,
-    senha_usuario NVARCHAR(255) NOT NULL,
+    nome_usuario VARCHAR(100) NOT NULL,
+    email_usuario VARCHAR(100) NOT NULL UNIQUE,
+    telefone_usuario VARCHAR(20) NULL,
+    senha_usuario VARCHAR(255) NOT NULL,
     biografia_usuario VARCHAR(255) NULL,
-    tipo_usuario ENUM('Escola','Comum','ADM'),
-    status_usuario INT DEFAULT 1 CHECK (status_usuario IN (0, 1)),
+    cor_banner VARCHAR(7) DEFAULT '#C7D3DD',
+    tipo_usuario ENUM('E','C','A'),
+    status_usuario INT DEFAULT 0 CHECK (status_usuario IN (0, 1)),
     img_perfil_id INT DEFAULT NULL,
-    FOREIGN KEY (img_perfil_id) REFERENCES imagens(id_imagem)
+    FOREIGN KEY (img_perfil_id) REFERENCES imagens(id_imagem),
+    
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+update usuarios set tipo_usuario = "A" where id_usuario = 1;
 
 DROP TABLE IF EXISTS escolas;
 CREATE TABLE escolas (
     id_escola INT PRIMARY KEY AUTO_INCREMENT,
     id_usuario INT NOT NULL,
-    nome_escola NVARCHAR(100) NOT NULL,
-    email_escola NVARCHAR(100) NOT NULL UNIQUE,
-    senha_escola NVARCHAR(255) NOT NULL,
+    nome_escola VARCHAR(100) NOT NULL,
+    email_escola VARCHAR(100) NOT NULL UNIQUE,
+    senha_escola VARCHAR(255) NOT NULL,
     cep CHAR(9) NOT NULL,
-    endereco NVARCHAR(255) NOT NULL,
+    endereco VARCHAR(255) NOT NULL,
     numero INT NOT NULL,
     cnpj CHAR(18) NOT NULL UNIQUE,
-    tipo_ensino NVARCHAR(255),
-    turnos NVARCHAR(255),
-    rede NVARCHAR(50),
+    tipo_ensino VARCHAR(255),
+    turnos VARCHAR(255),
+    rede VARCHAR(50),
     img_perfil_id INT DEFAULT NULL,
     FOREIGN KEY (img_perfil_id) REFERENCES imagens(id_imagem),
     whatsapp VARCHAR(20) NULL,
@@ -47,7 +53,11 @@ CREATE TABLE escolas (
     sobre_ensino TEXT NULL,
     sobre_estrutura TEXT NULL,
     ingresso VARCHAR(255) NULL,
-    FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario)
+	
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario),
+    
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS avaliacoes;
@@ -55,11 +65,13 @@ CREATE TABLE avaliacoes (
     id_avaliacao INT PRIMARY KEY AUTO_INCREMENT,
     id_usuario INT NOT NULL,
     id_escola INT NOT NULL,
-    comentario NVARCHAR(500),
+    comentario VARCHAR(500),
     data_avaliacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     nota INT CHECK (nota BETWEEN 1 AND 5),
     FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario),
-    FOREIGN KEY (id_escola) REFERENCES escolas(id_escola)
+    FOREIGN KEY (id_escola) REFERENCES escolas(id_escola),
+    
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS imagens_escola;
@@ -92,6 +104,69 @@ CREATE TABLE turnos (
     nome_turno VARCHAR(50) NOT NULL UNIQUE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+CREATE TABLE escola_tipos_ensino (
+    id_escola INT NOT NULL,
+    id_tipo_ensino INT NOT NULL,
+    PRIMARY KEY (id_escola, id_tipo_ensino),
+    FOREIGN KEY (id_escola) REFERENCES escolas(id_escola) ON DELETE CASCADE,
+    FOREIGN KEY (id_tipo_ensino) REFERENCES tipos_ensino(id_tipo_ensino)
+);
+
+CREATE TABLE escola_turnos (
+    id_escola INT NOT NULL,
+    id_turno INT NOT NULL,
+    PRIMARY KEY (id_escola, id_turno),
+    FOREIGN KEY (id_escola) REFERENCES escolas(id_escola) ON DELETE CASCADE,
+    FOREIGN KEY (id_turno) REFERENCES turnos(id_turno)
+);
+
+DROP TABLE IF EXISTS plano;
+CREATE TABLE plano (
+    id_plano int unsigned primary key auto_increment,
+    nome_plano varchar(15) not null,
+    preco_plano decimal(5,2) not null,
+    duracao_plano char(1) not null
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE assinatura (
+    id_assinatura INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    id_escola INT NOT NULL,
+    id_plano INT UNSIGNED NOT NULL,
+    data_inicio DATE NOT NULL,
+    data_fim DATE NULL,
+    ativo BOOLEAN DEFAULT TRUE,
+
+    FOREIGN KEY (id_escola) REFERENCES escolas(id_escola),
+    FOREIGN KEY (id_plano) REFERENCES plano(id_plano)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+DROP TABLE IF EXISTS denuncias;
+CREATE TABLE denuncias (
+    id_denuncia INT PRIMARY KEY AUTO_INCREMENT,
+
+    -- QUEM DENUNCIA
+    id_usuario_denunciante INT NULL,
+    id_escola_denunciante INT NULL,
+
+    -- QUEM É DENUNCIADO
+    id_usuario_denunciado INT NULL,
+    id_escola_denunciado INT NULL,
+
+    motivo VARCHAR(255) NOT NULL,
+    descricao TEXT NULL,
+    data_denuncia DATETIME DEFAULT CURRENT_TIMESTAMP,
+    status ENUM('P', 'A', 'R') DEFAULT 'P', -- P: pendente; A: Em analise; R: resolvida
+
+    FOREIGN KEY (id_usuario_denunciante) REFERENCES usuarios(id_usuario),
+    FOREIGN KEY (id_escola_denunciante) REFERENCES escolas(id_escola),
+    FOREIGN KEY (id_usuario_denunciado) REFERENCES usuarios(id_usuario),
+    FOREIGN KEY (id_escola_denunciado) REFERENCES escolas(id_escola),
+    
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+
 INSERT IGNORE INTO tipos_ensino (nome_tipo_ensino) VALUES
 ('Educação Infantil'),
 ('Ensino Fundamental I'),
@@ -108,3 +183,10 @@ INSERT IGNORE INTO turnos (nome_turno) VALUES
 ('Integral'),
 ('Noite'),
 ('Sábado');
+
+CREATE INDEX idx_turno_nome ON turnos(nome_turno);
+CREATE INDEX idx_tipo_ensino_nome ON tipos_ensino(nome_tipo_ensino);
+CREATE INDEX idx_denuncias_status ON denuncias(status);
+CREATE INDEX idx_escolas_nome ON escolas(nome_escola);
+CREATE INDEX idx_escolas_rede ON escolas(rede);
+CREATE INDEX idx_escolas_cep ON escolas(cep);
